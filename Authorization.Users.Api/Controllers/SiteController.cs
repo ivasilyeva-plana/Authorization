c9eee18bff45b0f05d206e833b9333f603a1279b
vasilyevainna <vasilyeva_inna@ukr.net>
2021-09-06 19:04:15 +0300

    IdentityServer4 Api projects

---
+++ Authorization.Users.Api/Controllers/SiteController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Authorization.Users.Api.Controllers
+{
+    [Route("[controller]")]
+    public class SiteController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SiteController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public async Task<IActionResult> GetOrders()
+        {
+            var ordersClient = _httpClientFactory.CreateClient();
+
+            //ordersClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await ordersClient.GetAsync("https://localhost:5001/site/secret");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                ViewBag.Message = response.StatusCode.ToString();
+                return View();
+            }
+
+            var message = await response.Content.ReadAsStringAsync();
+
+            ViewBag.Message = message;
+            return View();
+        }
+    }
+}
